8e2204373181a976d034be4bc16f9ce796c6d237
prashanth810 <137690380+prashanth810@users.noreply.github.com>
2024-12-02 19:58:08 +0530

    Update Productscontext.js

--- icecream/src/context/Productscontext.js
+++ icecream/src/context/Productscontext.js
@@ -1,32 +1,99 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
+export const Foodcontext = createContext();
 
-export const Productscontext = createContext();
+const FoodcontextPprovider = ({ children }) => {
+    const [token, setToken] = useState("");
+    const [food_list, setFood_list] = useState([]);
+    const [cartitems, setCartitems] = useState({});
 
-const ProductscontextProvider = (props) => {
-    const [vegitables_vegis, setVegitables_vegis] = useState([]);
+    const url = "https://icrecreambackend.onrender.com";
+
+    const handlefetchfood = async () => {
+        const response = await axios.get(url + "/api/food/list");
+        setFood_list(response.data.data);
+    }
+
+    const addtocart = async (itemId) => {
+        if (!cartitems[itemId]) {
+            setCartitems((prev) => ({
+                ...prev,
+                [itemId]: 1
+            }))
+        }
+
+        else {
+            setCartitems((prev) => ({ ...prev, [itemId]: prev[itemId] += 1 }));
+        }
+        if (token) {
+            try {
+                await axios.post(url + "/api/cart/add", { itemId }, { headers: { token } });
+            }
+            catch (error) {
+                console.log("adding error", error);
+            }
+        }
+    }
 
 
-    const fetchproducts = async () => {
-        const resp = await fetch(`http://localhost:8080/auth/products`);
-        const res = await resp.json();
-        setVegitables_vegis(res);
+    const removefromcart = async (itemId) => {
+        setCartitems((prev) => ({
+            ...prev,
+            [itemId]: prev[itemId] > 1 ? prev[itemId] - 1 : 0
+        }));
+
+        if (token) {
+            await axios.post(url + "/api/cart/remove", { itemId }, { headers: { token } });
+        }
+    };
+
+    const gettotalcart = () => {
+        let totalAmmount = 0;
+        for (const items in cartitems) {
+            if (cartitems[items] > 0) {
+                let itemInfo = food_list.find((product) => product._id === items);
+                totalAmmount += itemInfo * cartitems[items];
+            }
+        }
+        return totalAmmount;
+    }
+
+    const loadcartData = async (token) => {
+        const response = await axios.post(url + "/api/cart/list", {}, { headers: { token } });
+        setCartitems(response.data.cartData);
     }
 
     useEffect(() => {
-        fetchproducts();
+        async function loadData() {
+            await handlefetchfood();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+                await loadcartData(localStorage.getItem("token"));
+            }
+        }
+        loadData();
     }, []);
 
 
-    const contextvalues = {
-        vegitables_vegis,
-        setVegitables_vegis
+    const contextvalue = {
+        url,
+        token,
+        setToken,
+        food_list,
+        setFood_list,
+        addtocart,
+        removefromcart,
+        cartitems,
+        setCartitems,
+        gettotalcart,
     }
 
     return (
-        <Productscontext.Provider value={contextvalues}>
-            {props.children}
-        </Productscontext.Provider>
+        <Foodcontext.Provider value={contextvalue} >
+            {children}
+        </Foodcontext.Provider>
     )
 }
-export default ProductscontextProvider;
+
+export default FoodcontextPprovider;
